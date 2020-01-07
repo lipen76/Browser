f31744759de70ff16ab015f8a3df0682808903a7
lipen76 <sudnikdmitrij@gmail.com>
2020-01-07 23:05:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/myapplicationbrowser/myWebClient.java
@@ -0,0 +1,16 @@
+package com.example.android.myapplicationbrowser;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class myWebClient extends WebViewClient {
+
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
